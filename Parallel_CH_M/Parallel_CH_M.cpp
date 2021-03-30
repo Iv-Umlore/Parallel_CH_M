e690891e0807b0137b36e231bc2075cec33fced1
Iv-Umlore <iv.kiselev1998@yandex.ru>
2021-03-30 18:53:18 +0300

    First update - interface

--- Parallel_CH_M/Parallel_CH_M.cpp
+++ Parallel_CH_M/Parallel_CH_M.cpp
@@ -4,8 +4,58 @@
 #include <iostream>
 #include <omp.h>
 
+// Примерный размер блока
+// L2: Всего 256 Мб. Всего 3 блока
+// ~ 80 Мб -> ~ 10млн double -> ~ 3000 * 3000
+// Свободная кэш память - 16 Мб.
+
+// По условию задачи матрица должна быть не больше 1000, поэтому для текущей задачи возьму число 100 за сторону блока
+const int block_size = 100;
+
+// Создание симметричной положительноопределённой матрицы 
+// A11 A21T
+// A21 A22
+//
+double* GetMainSimetricMatrix(int size) {
+	double* result = new double[size*size];
+
+	// i - column, j - line
+	for (int j = 0; j < size; j++) {
+		for (int i = 0; i < j; i++)	
+			result[j * size + i] = std::rand() % 75;
+
+			result[j * size + j] = 76 + std::rand() % 25;
+	}
+
+	return result;
+}
+
+double * GetLPart(double* aMatrix, int mSize) {
+	double *  resultLPart = new double[mSize * mSize];
+
+	for (int i = 0; i < mSize; i++)
+		for (int j = 0; j < mSize; j++)
+			if (i != j)
+				resultLPart[i * mSize + j] = 0;
+
+	return resultLPart;
+}
+
+double* MatrixMyltiply(double* fMatrix, double* sMatrix) {
+	double *  resultLPart = new double[1];
+	return resultLPart;
+}
+
+double* DefaultDecomposition(double* pMatrix){
+	double *  resultLPart = new double[1];
+	return resultLPart;
+}
+
 int main()
 {
+
+
+
     std::cout << "Hello World!\n"; 
 }
 
